77b2a890bf31b4f8a06487131e25b97b6ad6c52b
Hannah-Lisa Whittle <whittlehannahlisa@gmail.com>
2022-04-04 22:47:21 -0500

    "Updated code"

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,14 +1,37 @@
 <template>
- <ul class="news__list">
- <li class="news__item">News item 1</li>
- <li class="news__item">News item 2</li>
- <li class="news__item">News item 3</li>
- </ul>
+    <div v-for="article in articles" class="card_container">
+        <div class="news_card">
+            <img :src="article.urlToImage" alt="news">
+            <div class="card_details">
+                <h6>{{ article.title }}</h6>
+                <p class="card-text">{{ article.description }}</p>
+            </div>
+        </div>
+    </div>
 </template>
+
 <script>
 export default {
- data() {
- return {};
- },
+    data() {
+        return {
+            articles: []
+        }
+    },
+    created() {
+        let self = this;
+        fetch('https://newsapi.org/v2/top-headlines?country=us', 
+{
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`
+    }
+})
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        self.articles = data.articles;
+    });
+ }
 };
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@ import NewsList from '@/components/NewsList.vue'
 export default {
     data() {
         return {
-            message: "Hello Hannah-Lisa!"
+            message: "Hello World! Welcome to VueJS"
         };
     },
     components: { NewsList }
@@ -11,15 +11,48 @@ export default {
 </script>
 
 <template>
-<NewsList />
     <div class="container">
       <div class="text-center">
         <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
         <h1>{{ message }}</h1>
       </div>
+      <NewsList />
     </div>
 </template>
 
 <style>
 /* Add any component specific styles here */
+
+.card_container{
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+  grid-gap: 3rem;
+  align-items: stretch;
+}
+
+.card_container img{
+  width: 300px;
+  height: 170px;
+}
+
+.card_container p {
+}
+
+.news_card{
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  border: 1px solid  gray;
+  border-radius: 5px;
+  box-shadow: none;
+  transition: all 0.3s ease-in-out;
+  padding-bottom: 20px;
+  width: 300px
+}
+
+.card_details{
+  padding: 15px;
+  display: flex;
+  flex-direction: column;
+}
 </style>
